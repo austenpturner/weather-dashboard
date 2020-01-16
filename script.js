c8917627bb0e3b4fcc51128a7b657fc3e9b7943f
Austen Turner <austenpturner@msn.com>
2020-01-16 14:33:13 -0800

    add geolocation to get current position and display weather and forecast at current position on page load

--- script.js
+++ script.js
@@ -15,21 +15,52 @@ const humidityCardEl = $('.card-humidity');
 // Weather dashboard 
 let date = moment().format('dddd, MMMM Do');
 let weatherAPIKey = '1c15bab08bfbee58f4e3567a79550403';
-let newLocation; 
+let currentLocation;
+let lon;
+let lat;
+let newLocation;
+let coordinatesWeatherURL;
 let weatherQueryURL;
 let forecastQueryURL;
+
+if (navigator.geolocation) {
+    console.log('yes');
+    navigator.geolocation.getCurrentPosition(displayLocationInfo);
+} else {
+    console.log('no');
+}
+
+function displayLocationInfo(position) {
+    lon = position.coords.longitude;
+    lat = position.coords.latitude;
+
+    coordinatesWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`
+    coordinatesForecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`;
+
+    $.ajax({
+        url: coordinatesWeatherURL,
+        method: `GET`
+    }).then (function(response){ 
+        renderConditions(response);           
+    });
+    $.ajax({
+        url: coordinatesForecastURL,
+        method: `GET`
+    }).then (function(response){
+        renderForecast(response);            
+    });
+}
 
 searchBtn.click(function(event) {
     event.preventDefault();
     newLocation = locationInput.val();
     weatherQueryURL = `https://api.openweathermap.org/data/2.5/weather?q=${newLocation}&appid=${weatherAPIKey}`;
     forecastQueryURL = 
-    `https://api.openweathermap.org/data/2.5/forecast?q=${newLocation}&APPID=${weatherAPIKey}`;
+    `https://api.openweathermap.org/data/2.5/forecast?q=${newLocation}&appid=${weatherAPIKey}`;
     $.ajax({
         url: weatherQueryURL,
         method: `GET`
-    }).then (function(response){
-        // console.log(response); 
+    }).then (function(response){ 
         renderConditions(response);            
     });
     $.ajax({
@@ -54,11 +85,9 @@ function renderForecast(city) {
         $(this).text(moment().add((i), 'days').format('dddd'));
     })
     tempCardEl.each(function(i) {
-        // console.log(city.list[i].main.temp);
         $(this).text(city.list[i].main.temp);
     })
     humidityCardEl.each(function(i) {
-        // console.log(city.list[i].main.humidity);
         $(this).text(city.list[i].main.humidity);
     })
 }
