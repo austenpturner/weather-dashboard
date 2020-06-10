73632d99adca664e0ebc5903d7ff0b1b3159861e
Austen Turner <austenpturner@msn.com>
2020-06-10 15:09:44 -0700

    add react nav slider

--- react-app/src/components/Header/Header.js
+++ react-app/src/components/Header/Header.js
@@ -10,11 +10,40 @@ class Header extends Component {
         };
     }
 
+    handleNavSlide() {
+        // const targetId = event.target.id;
+        if (this.state.slideNav) {
+            this.setState({ slideNav: false});
+        } else {
+            this.setState({ slideNav: true });
+        }
+    }
+
     render() {
         return (
             <div id="header">
                 <h1>Weather Dashboard</h1>
-                <Nav />
+                <div 
+                    id='nav-icon' 
+                    onClick={this.handleNavSlide.bind(this)}
+                >
+                    <div 
+                        className={this.state.slideNav ? "line toggle1" : "line"} 
+                        id='line1'
+                    ></div>
+                    <div 
+                        className={this.state.slideNav ? "line toggle2" : "line"} 
+                        id='line2'
+                    ></div>
+                    <div 
+                        className={this.state.slideNav ? "line toggle3" : "line"} 
+                        id='line3'
+                    ></div>
+                </div>
+                <Nav 
+                    handleNavSlide={this.handleNavSlide.bind(this)} 
+                    slideNav={this.state.slideNav}
+                />
             </div>
         );
     }
--- react-app/src/components/Header/Nav.js
+++ react-app/src/components/Header/Nav.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 class Nav extends Component {
     render() {
         return (
-        <div id="nav">
-            <h4>Nav</h4>
-        </div>
+            <nav  
+                className={this.props.slideNav ? "slide-in" : "slide-out"}
+            >
+                <ul 
+                    id='saved-list' 
+                    className='slide-right'
+                >
+                    <li>
+                        <a 
+                            className='saved-place' 
+                            href='#about'
+                            onClick={this.props.handleNavSlide}
+                        >
+                            About
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         );
     }
 }
